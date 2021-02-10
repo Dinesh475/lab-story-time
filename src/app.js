09ec4032c5563dbdcf4a36bac7bdb19f3481d56d
Dinesh475 <dineshbalaji475@gmail.com>
2021-02-10 13:00:07 +0530

    1938

--- src/app.js
+++ src/app.js
@@ -3,14 +3,41 @@
 //Debug the type of data provided
 //Return the types concatenated in a single variable
 
+moreAboutHome=(address,distanceFromTown,hasNeighbours)=>{
+    var str1 = typeof(address);
+    var str2 = typeof(distanceFromTown);
+    var str3 = typeof(hasNeighbours);
+    var res = str1.concat(str2, str3);
+    return res;
+}
+
+
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+moreAboutKaren=(parents, noOfSiblings, isNuclearFamily)=>{
+    if(typeof(parents)==="string" && typeof(noOfSiblings)==="number" && typeof(isNuclearFamily)==="boolean")
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+doesFriendExist=(ageInText, ageInNumber)=>{
+    if(isNaN(ageInText) || isNaN(ageInNumber)){
+        return NaN;
+    }
+}
+
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +46,29 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+sweetTooth=(x, y, z, n)=>{
+    if((typeof(x)==="string" || typeof(y)==="string" || typeof(z)==="string" || typeof(n)==="string") || (x===undefined))
+    {
+        return "No sweets for Karen's friend";
+    }
+    let rem_sweet = (x-y-(z*n))
+    return rem_sweet;    
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+convertToCelsius=(fahrenheit)=>{
+    if(typeof(fahrenheit)=== "number")
+    {
+        let celsius = (fahrenheit-32)*(5/9);
+        return celsius;
+    }
+    else{
+        return "Technical Error!";
+    }
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -30,6 +77,27 @@
 //3. Counsel her daughter herself
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
+
+aDifficultChoice=(choice)=>{
+    switch(choice){
+        case 1: text="Take her daughter to a doctor";
+                break;
+        case 2: text="Talk to her husband about it";
+                break;
+        case 3: text="Counsel her daughter herself";
+                break;
+        case 4: text="Lock her daughter in her room";
+                break;
+        case -1:text="Break down and give up all hope";
+                break;            
+        case undefined: text="Wasn't able to decide";
+                        break;
+        case "I give up": text="Refused to do anything for Karen";
+                            break;                                
+    }
+    return text;
+}
+
 
 //Progression 7:
 //Lily realized that she'd hurt her daughter
